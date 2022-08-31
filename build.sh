31ef5703e05b7d4a80273aeaa9b5716fb5500310
v-sukhov <52100863+v-sukhov@users.noreply.github.com>
2022-08-31 11:38:26 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export REACT_APP_VERSION=`cat version`
 mvn package
-cp -f .\magreport-backend\target\magreport-backend-2.0.jar .\package\magreport.jar
-cp -f .\magreport-backend\profiles\application.properties.template .\package\application.properties.template
-cp -f .\magreport-backend\profiles\logback.xml.template .\package\logback.xml.template
-cp -f .\docs\user-manual\src\user-manual.pdf .\package\user-manual.pdf
+cp -f ./magreport-backend/target/magreport-backend-2.0.jar ./package/magreport.jar
+cp -f ./magreport-backend/profiles/application.properties.template ./package/application.properties.template
+cp -f ./magreport-backend/profiles/logback.xml.template ./package/logback.xml.template
+cp -f ./docs/user-manual/src/user-manual.pdf ./package/user-manual.pdf
